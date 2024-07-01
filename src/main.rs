06153f5befa1075f2bee5ca6a179631da1bf8e1b
Victor Prokhorov <mail@victorprokhorov.com>
2024-07-01 09:18:09 +0200

    sipmlify structure

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use anyhow::Context;
-use anyhow::Error;
 use anyhow::Result;
 use serde::Deserialize;
 use serde::Serialize;
 use std::collections::HashMap;
-use std::fs::File;
 use std::io;
 use std::io::BufRead;
-use std::io::Read;
 use std::io::Write;
-use std::time::Instant;
+use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize)]
 struct Msg {
     src: String,
     dest: String,
@@ -19,24 +15,22 @@ struct Msg {
 }
 
 impl Msg {
-    fn send(self, wtr: &mut impl Write) -> Result<()> {
-        serde_json::to_writer(&mut *wtr, &self)?;
-        wtr.write_all(b"\n")?;
+    fn send(self, stdout: &mut impl Write) -> Result<()> {
+        serde_json::to_writer(&mut *stdout, &self)?;
+        stdout.write_all(b"\n")?;
         Ok(())
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize)]
 struct Body {
     #[serde(flatten)]
     pl: Pl,
-    #[serde(skip_serializing_if = "Option::is_none")]
     msg_id: Option<usize>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     in_reply_to: Option<usize>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Pl {
@@ -56,25 +50,12 @@ enum Pl {
         id: String,
     },
     Broadcast {
-        message: usize, // message client send
+        message: usize,
     },
     BroadcastOk,
-    Propagate {
-        message: usize, // mesage node send node to "broadcast"
-    },
-    PropagateOk {
-        message: usize, // message that node send to confirm he did received this message
-    },
-    // better name?
-    PropagateOkOk {
-        message: usize, // mesasge that the initial node will receiv allowing him to remove msg
-                        // from pending "queue"
-    },
     Read,
     ReadOk {
-        #[serde(skip_serializing_if = "Option::is_none")]
         messages: Option<Vec<usize>>,
-        #[serde(skip_serializing_if = "Option::is_none")]
         value: Option<usize>,
     },
     Topology {
@@ -87,32 +68,14 @@ enum Pl {
     AddOk,
 }
 
-impl TryFrom<String> for Msg {
-    type Error = Error;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        serde_json::from_str(&value).context("deserialize from text")
-    }
-}
-
-fn proc(
-    bufrdr: impl BufRead,
-    wtr: &mut impl Write,
-    node_id: &mut String,
-    counter: &mut usize,
-) -> Result<()> {
-    // todo: it feels like it's possible to find better data structure (hashmap that uses msg id?), but i will start with
-    // simple vector
-    let mut messages = Vec::new();
-    let mut t = HashMap::new();
-    for line in bufrdr.lines() {
+fn main() -> Result<()> {
+    let stdin = io::stdin().lock();
+    let mut stdout = io::stdout().lock();
+    for line in stdin.lines() {
         let line = line?;
-        // dbg!(&line);
-        let req: Msg = line.try_into()?;
+        let req: Msg = serde_json::from_str(&line)?;
         match req.body.pl {
-            Pl::Init { node_id: nid, .. } => {
-                *node_id = nid;
-                // dbg!(&node_id);
+            Pl::Init { .. } => {
                 let resp = Msg {
                     src: req.dest,
                     dest: req.src,
@@ -122,7 +85,7 @@ fn proc(
                         msg_id: None,
                     },
                 };
-                resp.send(wtr)?;
+                resp.send(&mut stdout)?;
             }
             Pl::Echo { echo } => {
                 let resp = Msg {
@@ -134,29 +97,22 @@ fn proc(
                         in_reply_to: req.body.msg_id,
                     },
                 };
-                resp.send(wtr)?;
+                resp.send(&mut stdout)?;
             }
             Pl::Generate => {
-                let mut f = File::open("/dev/urandom")?;
-                let mut buf = [0; 16];
-                f.read_exact(&mut buf)?;
-                let now = Instant::now();
-                let id = format!("{node_id}-{buf:?}-{counter}-{now:?}");
-                // dbg!(&id);
-                *counter += 1;
+                let id = Uuid::now_v7();
                 let resp = Msg {
                     src: req.dest,
                     dest: req.src,
                     body: Body {
-                        pl: Pl::GenerateOk { id },
+                        pl: Pl::GenerateOk { id: id.to_string() },
                         msg_id: req.body.msg_id,
                         in_reply_to: req.body.msg_id,
                     },
                 };
-                resp.send(wtr)?;
+                resp.send(&mut stdout)?;
             }
-            Pl::Topology { topology } => {
-                t = topology;
+            Pl::Topology { .. } => {
                 let resp = Msg {
                     src: req.dest,
                     dest: req.src,
@@ -166,10 +122,9 @@ fn proc(
                         in_reply_to: req.body.msg_id,
                     },
                 };
-                resp.send(wtr)?;
+                resp.send(&mut stdout)?;
             }
-            // received from client
-            Pl::Broadcast { message } => {
+            Pl::Broadcast { .. } => {
                 let resp = Msg {
                     src: req.dest.clone(),
                     dest: req.src.clone(),
@@ -179,105 +134,25 @@ fn proc(
                         in_reply_to: req.body.msg_id,
                     },
                 };
-                resp.send(wtr)?; // respond Ok to client
-                                 // naive solution for multi node broadcaast, i will send all messages to all nodes in topology
-                                 // todo: yup it's cheasy to assumes that messages are unique, realisticly it should rather
-                                 // rely on msg id
-                if messages.iter().find(|&&x| x == message).is_none() {
-                    messages.push(message);
-                    let nodes = t
-                        .get(node_id)
-                        .expect("we expect to have a valid topology being passed at init");
-                    for n in nodes {
-                        // eprintln!("sending message '{message}' to dest '{n}'");
-                        let resp = Msg {
-                            src: node_id.clone(),
-                            dest: n.to_string(),
-                            body: Body {
-                                pl: Pl::Propagate { message },
-                                msg_id: None,
-                                in_reply_to: None,
-                            },
-                        };
-                        resp.send(wtr)?;
-                    }
-                }
-            }
-            Pl::Propagate { message } => {
-                // eprintln!("propagate {message}");
-                let resp = Msg {
-                    src: req.dest.clone(),
-                    dest: req.src.clone(),
-                    body: Body {
-                        pl: Pl::PropagateOk { message },
-                        msg_id: None,
-                        in_reply_to: None,
-                    },
-                };
-                resp.send(wtr)?; // respond Ok to client
-            }
-            Pl::PropagateOk { message } => {
-                // eprintln!("propagate ok {message}");
-                let resp = Msg {
-                    src: req.dest.clone(),
-                    dest: req.src.clone(),
-                    body: Body {
-                        pl: Pl::PropagateOkOk { message },
-                        msg_id: None,
-                        in_reply_to: None,
-                    },
-                };
-                resp.send(wtr)?; // respond Ok to client
-            }
-            Pl::PropagateOkOk { message } => {
-                // eprintln!("full round trip of {message}");
-                // todo: should i rather create `State` struct?
-                let i = messages.iter().position(|x| *x == message);
-                if let Some(i) = i {
-                    messages.swap_remove(i);
-                }
-                // and now since we did received this confirmation we know that network is fine
-                // thus let's try to send all unsend message
-                let nodes = t
-                    .get(node_id)
-                    .expect("we expect to have a valid topology being passed at init");
-                for &message in &messages {
-                    for n in nodes {
-                        let resp = Msg {
-                            src: node_id.clone(),
-                            dest: n.to_string(),
-                            body: Body {
-                                pl: Pl::Propagate { message },
-                                msg_id: None,
-                                in_reply_to: None,
-                            },
-                        };
-                        resp.send(wtr)?;
-                    }
-                }
+                resp.send(&mut stdout)?;
             }
             Pl::Read => {
-                let mut m = None;
-                if !messages.is_empty() {
-                    m = Some(messages.clone());
-                }
                 let v = None;
                 let resp = Msg {
                     src: req.dest,
                     dest: req.src,
                     body: Body {
                         pl: Pl::ReadOk {
-                            messages: Some(messages.clone()),
+                            messages: None,
                             value: v,
                         },
                         msg_id: req.body.msg_id,
                         in_reply_to: req.body.msg_id,
                     },
                 };
-                resp.send(wtr)?;
+                resp.send(&mut stdout)?;
             }
-            Pl::Add { delta } => {
-                // dbg!(delta);
+            Pl::Add { .. } => {
                 let resp = Msg {
                     src: req.dest,
                     dest: req.src,
@@ -287,59 +162,10 @@ fn proc(
                         in_reply_to: req.body.msg_id,
                     },
                 };
-                resp.send(wtr)?;
+                resp.send(&mut stdout)?;
             }
-            _ => todo!(),
+            _ => panic!(),
         }
     }
     Ok(())
-}
-
-fn main() -> Result<()> {
-    dbg!("node start");
-    let mut counter = 0;
-    let mut node_id = String::new();
-    let stdin = io::stdin().lock();
-    let mut stdout = io::stdout().lock();
-    proc(stdin, &mut stdout, &mut node_id, &mut counter)?;
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_init() {
-        let mut counter = 0;
-        let mut node_id = String::new();
-        let input = io::Cursor::new(
-            r#"{"src":"c0","dest":"n0","body":{"type":"init","msg_id":1,"node_id":"n3","node_ids":["n1","n2","n3"]}}"#,
-        );
-        let mut output = Vec::new();
-
-        proc(input, &mut output, &mut node_id, &mut counter).unwrap();
-
-        assert_eq!(
-            String::from_utf8(output).unwrap().trim(),
-            r#"{"src":"n0","dest":"c0","body":{"type":"init_ok","in_reply_to":1}}"#
-        );
-    }
-
-    #[test]
-    fn test_echo() {
-        let mut counter = 0;
-        let mut node_id = String::new();
-        let input = io::Cursor::new(
-            r#"{"src":"c1","dest":"n1","body":{"type":"echo","msg_id":1,"echo":"Please echo 35"}}"#,
-        );
-        let mut output = Vec::new();
-
-        proc(input, &mut output, &mut node_id, &mut counter).unwrap();
-
-        assert_eq!(
-            String::from_utf8(output).unwrap().trim(),
-            r#"{"src":"n1","dest":"c1","body":{"type":"echo_ok","echo":"Please echo 35","msg_id":1,"in_reply_to":1}}"#
-        );
-    }
 }
